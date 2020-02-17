26c77cdcf2630fa0f1816fbd9b9c83969b2182fd
Taora Kaniela <kaniela.taora@hotmail.fr>
2020-02-16 16:58:16 -1000

    admin screen, create group et categorie page done, starting php database connection

---
+++ php/categoriecreate.php
@@ -0,0 +1,31 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=rankit", "root", "");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+ 
+// Attempt insert query execution
+try{
+    // Create prepared statement
+    $sql = "INSERT INTO categories (name, color) VALUES (:name, :color)";
+    $stmt = $pdo->prepare($sql);
+    
+    // Bind parameters to statement
+    $stmt->bindParam(':name', $_REQUEST['name']);
+    $stmt->bindParam(':color', $_REQUEST['color']);
+    
+    // Execute the prepared statement
+    $stmt->execute();
+    echo "Records inserted successfully.";
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+ 
+// Close connection
+unset($pdo);
+?>
